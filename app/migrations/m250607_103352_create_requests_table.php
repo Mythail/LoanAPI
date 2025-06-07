6ae5988e55fba9e2434eb6fdc98a22ce63595315
Michael2031 <michaelr2031@gmail.com>
2025-06-07 13:47:50 +0300

    Добавление миграции для создания таблицы `requests`

---
+++ app/migrations/m250607_103352_create_requests_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `{{%requests}}`.
+ */
+class m250607_103352_create_requests_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('{{%requests}}', [
+            'id' => $this->primaryKey(),
+            'user_id' => $this->integer()->notNull(),
+            'amount' => $this->integer()->notNull(),
+            'term' => $this->integer()->notNull(),
+            'status' => $this->string()->null(),
+            'created_at' => $this->timestamp()->defaultExpression('CURRENT_TIMESTAMP'),
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropTable('{{%requests}}');
+    }
+}
